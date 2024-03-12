9916cf10872d4cf392421cfa0ffd874243b23530
mysterion <49054698+mysterion@users.noreply.github.com>
2024-03-12 16:33:08 +0530

    _

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-find . -type f -execdir tree -H '.' -L 1 --noreport --dirsfirst -T . -s -D --charset utf-8 -I "index.html" -o index.html {} \;
+find . -type d  -not -path "./.git/*" -exec sh -c 'cd -P -- "{}" && tree -H '.' -L 1 --noreport --dirsfirst -T . -s -D --charset utf-8 -I "index.html" -o index.html' \;
 git add .
 git commit -m'_'
 git push
